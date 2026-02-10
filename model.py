5da1e7ef53a082b430fdcd780387128870fc2569
dastolme <melba.dastolfo@gmail.com>
2026-02-10 16:57:59 +0100

    Move back to one hidden layer

--- model.py
+++ model.py
@@ -10,7 +10,6 @@ class GraphEncoder(nn.Module):
         super().__init__()
         self.conv1 = GCNConv(node_in_dim, hidden_dim)
         self.conv2 = GCNConv(hidden_dim, hidden_dim)
-        self.conv3 = GCNConv(hidden_dim, hidden_dim)
         self.lin = nn.Linear(hidden_dim, out_dim)
     
     def forward(self, x, edge_index, batch):
@@ -18,9 +17,6 @@ class GraphEncoder(nn.Module):
         x = F.relu(x)
 
         x = self.conv2(x, edge_index)
-        x = F.relu(x)
-
-        x = self.conv3(x, edge_index)
         x = F.relu(x)
 
         g = global_mean_pool(x, batch)
